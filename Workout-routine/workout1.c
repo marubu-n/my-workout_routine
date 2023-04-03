87c0f4aad87435f8d4c65c8b4b731442b1bf469b
marubu-n <rodahnjoki70@gmail.com.com>
2023-04-03 06:50:47 -0400

    My workout files

---
+++ Workout-routine/workout1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Description: Print two strings
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Welcome!\n");
+	printf("You are able to test your skill of writing C code here");
+
+	return(0);
+}
---
+++ Workout-routine/workout2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *Description: Print out statement that prints name, age, dob and mobile number
+ *Return: Always 0
+ */
+int main(void)
+{
+
+	printf("Name: Rodah Njoki \n");
+	printf("Age: 25\n");
+	printf("Date of birth: Dec 2, 1988\n");
+	printf("Mobile number: 070064362");
+
+
+	return(0);
+}
